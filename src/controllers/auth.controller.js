c294e9b4545f67de6ae9e4dcbb089dc77c08176b
Arthur Bryan <arthurbryansk@gmail.com>
2022-12-01 15:04:52 -0300

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,20 @@
 const clientPromise = require("../services/db.service");
-const User = require("../models/user.model")
+const getCollection = require("../services/collection.service");
+const {
+  hashPassword,
+  verifyPasswordWithHash,
+} = require("../services/password.service");
+const { createToken, verifyToken } = require("../services/jwt.service");
+const User = require("../models/user.model");
 
 async function login(req, res) {
-
   const { user, password } = req.body;
-  const client = await clientPromise;
-  const db = client.db("auth");
-  const collection = db.collection("users");
+
+  const collection = await getCollection(clientPromise, "auth", "users");
 
   if (!user) {
     return res.status(404).json({ msg: "User not found" });
-  };
+  }
 
   if (!password) {
     return res.status(404).json({ msg: "Password is required." });
@@ -20,57 +24,59 @@ async function login(req, res) {
 
   if (!userExist) {
     return res.status(404).json({ message: "Non-existent user." });
-  };
+  }
 
   const dbUser = await collection.findOne({ user: user });
-  const checkPassword = password === dbUser.password;
+
+  const checkPassword = await verifyPasswordWithHash(password, dbUser.password);
 
   if (!checkPassword) {
     return res.status(422).json({ msg: "Invalid password." });
-  };
+  }
 
   try {
-    res.status(200).json({ msg: "Successful authentication." });
-  } catch (err) {
-    res.status(500).json({ msg: err });
-  };
-
-};
+    const token = await createToken({
+      user: dbUser.user,
+    });
+    res.status(200).json({ msg: "Autenticação realizada com sucesso!", token });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+}
 
 async function register(req, res) {
-
   const { user, password } = req.body;
-  const client = await clientPromise;
-  const db = client.db("auth");
-  const collection = db.collection("users");
+
+  const collection = await getCollection(clientPromise, "auth", "users");
 
   if (!user) {
     return res.status(422).json({ message: "User is required." });
-  };
+  }
 
   if (!password) {
     return res.status(422).json({ message: "Password is requires." });
-  };
+  }
 
   const userExist = await collection.findOne({ user: user });
 
   if (userExist) {
     return res.status(422).json({ message: "The user already exists." });
-  };
+  }
 
+  const hashedPassword = await hashPassword(password);
   const newUser = new User({
-    user,
-    password,
+    user: user,
+    password: hashedPassword,
   });
+  console.log(hashedPassword);
 
   try {
     await collection.insertOne(newUser);
     res.status(201).json({ msg: "User created successfully" });
   } catch (err) {
     res.status(500).json({ msg: err });
-  };
-
-};
+  }
+}
 
 module.exports = {
   login,
