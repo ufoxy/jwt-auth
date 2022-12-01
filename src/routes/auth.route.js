8afdf2af5333c1e4437dfcb45a5de5963e2225b6
Arthur Bryan <arthurbryansk@gmail.com>
2022-12-01 16:58:28 -0300

    Update auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const { verifyRefreshToken } = require("../services/jwt.service")
 
 router.post("/login", authController.login);
 router.post("/register", authController.register);
+router.post("/refresh", verifyRefreshToken, authController.refresh)
 
 module.exports = router;
